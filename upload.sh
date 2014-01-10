a13ed083e9aee8804081e3663ef4238393c21117
openwrt <jesstr@mail.ru>
2014-01-10 15:18:24 +0400

    [upload.sh] port fixed

--- upload.sh
+++ upload.sh
@@ -9,4 +9,4 @@ FLASHFILE=./Release/termo_rs485.hex
 sudo avrdude -p $MK -c $PRG -b $BAUD -P $PORT -U flash:w:$FLASHFILE
 
 # Manual run:
-# avrdude -p m8 -c avr910 -b 57600 -P /dev/ttyACM03 -U flash:w:"./Release/termo_rs485.hex":i
+# sudo avrdude -p m8 -c avr910 -b 57600 -P /dev/ttyACM0 -U flash:w:"./Release/termo_rs485.hex":i
